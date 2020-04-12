80b0f411b838842a869df309935ed7f73c544e80
David Peredo <daveperedo@gmail.com>
2020-04-12 23:00:46 +0000

    Added watchlist.

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,45 +2,53 @@ import { useState } from "react";
 import { createContainer } from "unstated-next";
 
 export interface Quote {
-    symbol: string,
-    open: number,
-    close: number,
-    lastPrice: number,
-    change: number,
-    changePercent: number
+  tickerSymbol: string,
+  open: number,
+  close: number,
+  lastPrice: number,
+  change: number,
+  changePercent: number
+}
+
+export interface Watchlist {
+  tickerSymbols: string[]
 }
 
 export const useStore = () => {
-    const tempDefault = [
-        {
-            symbol: "SPY",
-            open: 200,
-            close: 180,
-            lastPrice: 180,
-            change: 20,
-            changePercent: 10
-        },
-        {
-            symbol: "TSLA",
-            open: 200,
-            close: 180,
-            lastPrice: 180,
-            change: 20,
-            changePercent: 10
-        },
-        {
-            symbol: "VIX",
-            open: 200,
-            close: 180,
-            lastPrice: 180,
-            change: 20,
-            changePercent: 10
-        }
-    ];
+  const tempDefault = [
+    {
+      tickerSymbol: "SPY",
+      open: 200,
+      close: 180,
+      lastPrice: 180,
+      change: 20,
+      changePercent: 10
+    },
+    {
+      tickerSymbol: "TSLA",
+      open: 200,
+      close: 180,
+      lastPrice: 180,
+      change: 20,
+      changePercent: 10
+    },
+    {
+      tickerSymbol: "VIX",
+      open: 200,
+      close: 180,
+      lastPrice: 180,
+      change: 20,
+      changePercent: 10
+    }
+  ];
 
-    const [quotes, setQuotes] = useState<Quote[]>(tempDefault);
+  const [quotes, setQuotes] = useState<Map<string, Quote>>(new Map<string, Quote>());
+  const [watchlist, setWatchlist] = useState<Watchlist>();
 
-    return { quotes, setQuotes };
+  const setQuote = (quote) => {
+    setQuotes()
+  };
+  return { quotes, setQuotes };
 };
 
 export const QuotesContainer = createContainer(useStore);
